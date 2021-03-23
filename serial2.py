8672996cc929cb41b0de3f8e6f5333b5a1da26c1
Jeff Smith <jsmith@conflexion.com>
2021-03-23 14:50:09 -0500

    Create serial2.py
    Two-station serial line.

---
+++ serial2.py
@@ -0,0 +1,27 @@
+# -----------------
+# serial1.py - Serial line with M/M/c stations
+#
+# Jeff Smith - 2016-12-20
+#
+# -----------------
+import sys
+import mmc
+
+# lambda - arrival rate
+l = 15
+# station service rates (mu) and capacities (c)
+mus = [6, 18]
+cs  = [3,  1]
+
+# solve
+sol = []
+for i in range(len(mus)):
+	sol.append(mmc.mmc(l, mus[i], cs[i]))
+	if sol[i]['rho'] >= 1.0:
+		print ("System unstable (station {:})".format(i+1))
+		sys.exit()
+	else:
+		mmc.show(sol[i], "Station {:}".format(i+1))
+L = sum([s['L'] for s in sol])
+W = sum([s['W'] for s in sol])
+print ("\nNIS: {:.3f}, TIS: {:.3f}".format(L, W))
